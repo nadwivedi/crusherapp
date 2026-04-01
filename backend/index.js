3f1e711b8547d9315299fb5282bed647209a4b2a
Narayandwivedi <desinplus1@gmail.com>
2026-04-01 11:32:35 +0530

    fix cors

--- backend/index.js
+++ backend/index.js
@@ -27,7 +27,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 const corsOptions = {
-  origin: ["http://localhost:5173", "http://localhost:5174", "http://localhost:5175"],
+  origin: ["http://localhost:5173", "http://localhost:5174", "http://localhost:5175", "https://app.crusherbook.com", "https://crusherbook.com"],
   credentials: true,
 };
 
